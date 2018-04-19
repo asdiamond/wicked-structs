50d830e7bfb82c053647ea5ec785f4ba4e0b40e8
aleksandr <asdiamond@miners.utep.edu>
2018-04-18 21:44:20 -0600

    Completed node class with documentation.

--- src/main/java/DoublyLinkedList.java
+++ src/main/java/DoublyLinkedList.java
@@ -6,12 +6,61 @@ import com.sun.javaws.exceptions.InvalidArgumentException;
  * elements
  */
 public class DoublyLinkedList<T> {
-    class Node{
+    /**
+     * A Doubly Linked List Node
+     * @param <T> The type of the data
+     */
+    private static class Node<T>{
 
+        /**The Next node*/
+        private Node<T> next;
+
+        /**The Prev node*/
+        private Node<T> prev;
+
+        /**The Data*/
+        private T data;
+
+
+        /**
+         * Constructor for Node
+         * @param data The data of this node
+         */
+        private Node(T data){
+            this(null, null, data);
+        }
+
+        /**
+         * Constructs a node
+         * @param next The next node
+         * @param prev The prev node
+         * @param data The data of this node
+         */
+        private Node(Node<T> next, Node<T> prev, T data){
+            this.next = next;
+            this.prev = prev;
+            this.data = data;
+        }
+
+        private Node<T> getNext() {
+            return next;
+        }
+
+        private void setNext(Node<T> next) {
+            this.next = next;
+        }
+
+        private Node<T> getPrev() {
+            return prev;
+        }
+
+        private void setPrev(Node<T> prev) {
+            this.prev = prev;
+        }
     }
 
-    private Node head;
-    private Node tail;
+    private Node<T> head;
+    private Node<T> tail;
 
     public DoublyLinkedList(){
         head = null;
